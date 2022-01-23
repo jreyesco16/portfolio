d6c5740db64e3188c05867153a0772e606bf0649
jreyesco16 <jesserc.2@gmail.com>
2022-01-22 22:38:41 -0600

    mvp for portfolio

---
+++ components/Education.tsx
@@ -0,0 +1,13 @@
+const Education = () => {
+    return (
+        <div>
+            <h3>Education</h3>
+            <span>Aug 2013 - Dec 2020 University of Nebraska - Lincoln (UNL)</span> <br/>
+            <span>Bachelors in Arts and Science</span> <br/>
+            <span>Major: Computer Science</span><br/>
+            <span>Minor: Mathematics</span><br />
+        </div>
+    )
+}
+
+export default Education;
---
+++ components/Languages.tsx
@@ -0,0 +1,12 @@
+const Languages = () => {
+    return (
+        <div>
+            <h3>Lanaguages</h3>
+            <span>Javascript</span> <br />
+            <span>Python</span> <br />
+            <span>C/C++</span> <br />
+        </div>
+    )
+}
+
+export default Languages;
