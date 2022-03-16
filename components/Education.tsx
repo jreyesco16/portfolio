20fe4fabfa8fdb439584765e59eccf94a33a6c3f
jreyesco16 <jesserc.2@gmail.com>
2022-03-16 18:16:23 -0500

    updated education information

--- components/Education.tsx
+++ components/Education.tsx
@@ -7,16 +7,14 @@ const Education = () => {
     return (
         <div className={styles.body}>
             <h2>Education</h2>
-            <Image src={"/unl.png"} height={60} width={140} layout={"fixed"} alt={"unl logo"} /> <br/>
-                <Space direction="vertical">
-                    <span>University of Nebraska - Lincoln (UNL) <br/> Aug 2013 - Dec 2020 </span>
-                </Space>
-                <Space direction="vertical">
-                    <span>
-                        <strong>Bachelors:</strong> Arts and Science <br/>
-                        <strong>Major: </strong> Computer Science <strong>Minor:</strong> Mathematics
-                    </span>
-                </Space>
+            <Space direction="vertical" className={styles.space}>
+                <Image src={"/unl-big.png"} height={70} width={210} layout={"fixed"} alt={"unl logo"} />
+                <span>
+                    <span><strong>Attened: </strong> Fall 2013 - Fall 2020 </span> <br/>
+                    <strong>Bachelors:</strong> Arts and Science <br/>
+                    <strong>Major: </strong> Computer Science <strong>Minor:</strong> Mathematics
+                </span>
+            </Space>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1> Jesse R. Cortes </h1>
+      <h1> Jesse R Cortes </h1>
 
       <Experience />
 
