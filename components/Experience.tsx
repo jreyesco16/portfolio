0bfee46895041353c41c56d9b3ce0861f8aa6d4a
jreyesco16 <jesserc.2@gmail.com>
2022-07-01 22:54:53 -0500

    wrapped experience section to be dynamic on browser resize

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,58 +1,61 @@
-import { Space } from "antd";
+import { Space, Typography, Card } from "antd";
 import Image from "next/image";
 import styles from "./Experience.module.css";
+
+const { Title, Text, Paragraph } = Typography;
+const { Meta } = Card;
 
 const Experience = () => {
   return (
     <div className={styles.body}>
-      <h2>
-        <strong>Experience</strong>
-      </h2>
-      <Image
-        src={"/freewater-logo.png"}
-        height={50}
-        width={180}
-        layout={"fixed"}
-        alt={"freewater logo"}
-      />
-      <br />
-      <span>
-        <strong>Frontend Developer</strong>: <strong>Freewater</strong>
-        <br />
-        June 2022 - Current
-      </span>
-      <br /> <br />
-      <Image
-        src={"/snyders-logo.png"}
-        height={70}
-        width={200}
-        layout={"fixed"}
-        alt={"snyders salvage logo"}
-      />
-      <br />
-      <span>
-        <strong>Software Developer</strong>: <strong>Snyders Salvage</strong>
-        <br />
-        June 2021 - Current
-      </span>
-      <br /> <br />
-      <a href="https://www.youtube.com/watch?v=czUheX_SWfk">
+      <Title level={3}>Experience</Title>
+      <Space
+        direction={"vertical"}
+        style={{ padding: "1rem 2rem", width: "350px" }}
+      >
         <Image
-          src={"/regular-phil.png"}
-          height={50}
-          width={200}
+          src={"/freewater-logo.png"}
+          height={60}
+          width={180}
           layout={"fixed"}
-          alt={"regular phil logo"}
-        />{" "}
-        <br />
-      </a>
-      <span>
-        <strong>Software Developer</strong>: <strong>Regular Phil</strong> -
-        DMSI (UNL Senior Design)
-      </span>{" "}
-      <br />
-      <span>August 2019 - May 2020</span>
-      <br /> <br />
+          alt={"freewater logo"}
+        />
+        <Title level={5}>Frontend Developer: Freewater</Title>
+        <Text strong>June 2022 - Current</Text>
+        <Paragraph></Paragraph>
+      </Space>
+      <Space
+        direction={"vertical"}
+        style={{ padding: "1rem 2rem", width: "350px" }}
+      >
+        <Image
+          src={"/snyders-logo.png"}
+          height={60}
+          width={180}
+          layout={"fixed"}
+          alt={"snyders salvage logo"}
+        />
+        <Title level={5}>Software Developer: Snyders Salvage</Title>
+        <Text strong>June 2021 - Current</Text>
+        <Paragraph></Paragraph>
+      </Space>
+      <Space
+        direction={"vertical"}
+        style={{ padding: "1rem 2rem", width: "350px" }}
+      >
+        <a href="https://www.youtube.com/watch?v=czUheX_SWfk">
+          <Image
+            src={"/regular-phil.png"}
+            height={60}
+            width={180}
+            layout={"fixed"}
+            alt={"regular phil logo"}
+          />
+        </a>
+        <Title level={5}>Software Developer: Regular Phil</Title>
+        <Text strong>August 2019 - May 2020</Text>
+        <Paragraph>Senior Design Project (UNL)</Paragraph>
+      </Space>
     </div>
   );
 };
