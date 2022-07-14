62dc7ae8be16950271328271561ae46e2477eea6
jreyesco16 <jesserc.2@gmail.com>
2022-07-13 23:06:10 -0500

    updated snyders experience section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,23 +1,37 @@
-import styles from "./Projects.module.css"
+import styles from "./Projects.module.css";
 
 const Projects = () => {
-    return (
-        <div className={styles.body}>
-            <h2><strong>Projects</strong></h2>
-
-            {/* MARKET */}
-            <span><strong>Market</strong> <br/>
-             Feb 2021 - Current</span> <br/>
-
-            {/* YAHTZEE ONLINE */}
-             <span><strong>Yahtzee Online</strong></span> <br />
-             Jan 2021 - May 2021 <br/>
-
-            {/* INVOICE SYSTEM */}
-             <span><strong>Invoice System</strong></span> <br />
-             August 2018 - December 2018
-        </div>
-    )
-}
+  return (
+    <div className={styles.body}>
+      <h2>
+        <strong>Projects</strong>
+      </h2>
+      {/* PORTFOLIO */}
+      <span>
+        Portfolio (<strong>NextJS</strong>) January 2022-Preset
+        <br />
+      </span>
+      <br />
+      {/* MARKET */}
+      <span>
+        Market (<strong>React</strong> + <strong>Django</strong>) Feb 2021 -
+        Current
+        <br />
+      </span>
+      <br />
+      {/* YAHTZEE ONLINE */}
+      <span>
+        Yahtzee Online (<strong>Java</strong>) Jan 2021 - May 2021
+        <br />
+      </span>
+      <br />
+      {/* INVOICE SYSTEM */}
+      <span>
+        Invoice System (<strong>Java</strong>) August 2018 - December 2018
+      </span>
+      <br />
+    </div>
+  );
+};
 
 export default Projects;
