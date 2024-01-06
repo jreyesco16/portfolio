91ecdcf1f02a20181aa34d43c57bacb9a4e717b4
Jesse R Cortes <jesserc.2@gmail.com>
2024-01-06 17:37:27 -0600

    swapped next for sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Experience from "components/Experience.svelte";
+	import Projects from "components/Projects.svelte";
+	import Education from "components/Education.svelte";
+	import Aboutme from "components/Aboutme.svelte";
+</script>
+
+<svelte:head>
+	<title>Jesse R Cortes</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<body>
+	<Experience />
+	<Education />
+	<Projects />
+	<Aboutme />
+</body>
+
+
+<style>
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+
+</script>
+
+<header>
+	<nav>
+		<a href="#experience">Experience</a>
+		<a href="#education">Education</a>
+		<a href="#projects">Projects</a>
+		<a href="#aboutme">About Me</a>
+	</nav>
+</header>
+
+<style>
+	nav {
+		display: flex;
+		justify-content: space-around;
+		font-size: large;
+	}
+	a {
+		text-decoration: none;
+		color: black;
+	}
+	a:hover {
+		color: grey;
+	}
+	
+</style>
