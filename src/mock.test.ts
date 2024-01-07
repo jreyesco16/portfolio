e1d1b0507f882b749b846f0f840e6388069f5598
Jesse R Cortes <jesserc.2@gmail.com>
2024-01-06 19:49:15 -0600

    ran prettier format

--- src/mock.test.ts
+++ src/mock.test.ts
@@ -1,8 +1,7 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from 'vitest';
 
 describe('mock', () => {
-    it('passing', () => {
-        expect(1).toBe(1)
-    })
-}
-)
+	it('passing', () => {
+		expect(1).toBe(1);
+	});
+});
